cfd3aabfca19a365d6519b01dfc9d971340acc93
thgauravrajput <jsrjikh@gmail.com>
2025-09-18 09:49:26 +0530

    saparate navbars

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
+gsap.utils.toArray(".card").forEach((card, i) => {
+  gsap.from(card, {
+    opacity: 0,
+    x: -20,
+    scale: 0.6,
+    duration: 0.5,
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: card,
+      start: "top 80%",
+      end: 'top 70%',
+      markers: true,
+      scrub: 2
+    }
+  });
+});
 
+gsap.utils.toArray(".animate").forEach((card, i) => {
+  gsap.from(card, {
+    opacity: 0,
+    scale: 0.6,
+    duration: 0.5,
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: card,
+      start: "top 100%",
+      markers: true,
+    }
+  });
+});
 
-// main scripting 
+// // main scripting 
 
-let main = document.getElementById('main');
-let loader = document.getElementById('loader').classList;
-let currentLink = document.querySelector('.nav-link-active');
-console.log(currentLink)
+// let main = document.getElementById('main');
+ let loader = document.getElementById('loader').classList;
+// let currentLink =document.querySelector('.nav-link-active');
+// console.log(currentLink);
+// function loadPage(link) {
 
-function loadPage(link) {
-  loader.remove('d-none')
-  fetch(link).then(res => res.text()).then(res => {
-    main.innerHTML = res;
-    window.scrollTo(0,0);
-  }).catch(e => {
-    main.innerHTML = `Something Went Wrong. Please Try Again Later`
-  }).finally(() => {
-    loader.add('d-none');
-    document.querySelectorAll('.nav-link').forEach(link => {
-      link.addEventListener('click', event => {
-        event.preventDefault();
-        //  console.log("Link clicked!", new URL(event.target.href).pathname);
-        currentLink.classList.remove('nav-link-active')
-        currentLink = event.target;
-        currentLink.classList.add('nav-link-active')
-        loadPage(new URL(event.target.href).pathname);
-      })
-    });
-
-
-  })
-
+//fetching navbar
+if(document.getElementById('navbar')){
+loader.remove('d-none')
+fetch('navbar.html').then(res => res.text()).then(res => {
+  document.getElementById('navbar').innerHTML = res;
+}).catch(e => {
+  main.innerHTML = `Something Went Wrong. Please Try Again Later`
+}).finally(() => {
+  //loader.add('d-none'); 
+})
 }
+// }
 
-
-loadPage(new URL(currentLink.href).pathname);
+// console.log(new URL(location.href).searchParams.get('page'))
+// loadPage(new URL(location.href).searchParams.get('page')|| new URL(currentLink.href).pathname);
 
 
